41ffce9cdbabcaa8ec181e422f088a4900fe453d
Semyon Danilov <samvimes@yandex.ru>
2021-10-05 20:33:39 +0300

    что делали на паре

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,12 @@
 import "./app-thirdparty";
 import "../styles/calculator.less";
+
+$(function () {
+    let button7 = document.getElementById("button-7");
+
+    button7.onclick = function () {
+        alert("Button 7 was clicked.");
+    };
+
+
+});
